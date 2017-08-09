5c51b2ec511fe0ce3dd9488bd9bcb6000490f00c
Paul Hammerschmidt <hammerschmidtpaul@gmail.com>
2017-08-09 18:18:30 -0500

    create session route. new session form (login) that posts to session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
   def new
-    # @user = User.find_by
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user = @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  resources :sessions, only: [:new, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: [:index, :edit, :update]
 end
