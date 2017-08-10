437f59a4d48ab76e24eb23aa435fff4dd81b5475
Matthew Cataldi <mcataloe@gmail.com>
2017-08-10 09:42:15 -0500

    Adjust show page for article and add model methods to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,12 @@ class Article < ApplicationRecord
   accepts_nested_attributes_for :versions
 
   validates_presence_of :title, :versions
+
+  def latest_version
+    versions.order("created_at").last
+  end
+
+  def prior_versions
+    versions.order("created_at")[0..-2]
+  end
 end
