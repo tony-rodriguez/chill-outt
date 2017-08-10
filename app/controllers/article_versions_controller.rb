cd5f4034e820c29ed268464ca613120143b57270
Tony Rodriguez <email": null>
2017-08-10 15:55:13 -0500

    Add controller spec and tests for article versions

--- app/controllers/article_versions_controller.rb
+++ app/controllers/article_versions_controller.rb
@@ -1,14 +1,8 @@
 class ArticleVersionsController < ApplicationController
 
   def show
-    p "*" * 40
-    p params
     @article = Article.find(params[:article_id])
-    p @article
     @shown_version = @article.versions.find(params[:id])
-    p @shown_version
   end
-
-  private
 
 end
