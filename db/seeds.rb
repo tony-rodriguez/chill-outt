94e41bfc7ed33f25900ddb7a630fedf5063e777f
Tony Rodriguez <tonyr923@gmail.com>
2017-08-10 21:07:37 -0500

    Adjust seed file and perform migrations to change schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,18 +29,18 @@ articles_needed = articles_desired - articles_entered
 articles_needed.times do
   article = Article.new(
     is_featured: (rand(0..1)),
-    title: Faker::Hipster.sentence(rand(5..10)), # move this to line 43.5 when migrations have been changed
     )
 
   article.save(validate: false)
 
   versions_desired = rand(3..5)
-  versions_entered = versions.count
+  versions_entered = article.versions.count
   versions_needed = versions_desired - versions_entered
 
   versions_needed.times do
     article_version = ArticleVersion.new(
       content: Faker::Hipster.paragraph(rand(1..3)),
+      title: Faker::Hipster.sentence(rand(5..10)),
       article: article,
       author: User.all.sample,
       is_draft: rand(0..1)
